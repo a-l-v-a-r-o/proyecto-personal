1b16261805730e44246f644247d127b0632ba731
LAPTOP-LQIII0K7\alvar <alvaropherez@gmail.com>
2021-04-21 00:55:52 +0200

    Pequeños cambios

--- app/src/main/java/com/alpehe/huertovalenciano/MainActivity.java
+++ app/src/main/java/com/alpehe/huertovalenciano/MainActivity.java
@@ -3,12 +3,32 @@ package com.alpehe.huertovalenciano;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
+
+    ProgressBar circulo;
+    Button comenzar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        comenzar = (Button) findViewById(R.id.btnComenzar);
+        circulo = (ProgressBar) findViewById(R.id.progressBar);
+
+        comenzar.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                comenzar.setVisibility(View.INVISIBLE);
+                circulo.setVisibility(View.VISIBLE);
+            }
+        });
     }
 }
