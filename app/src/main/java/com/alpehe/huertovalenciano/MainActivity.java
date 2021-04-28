a42b1fc33a540176dee4f82530ebb458b2bd2f5a
aperezh <aperezh@obremo.es>
2021-04-28 13:01:21 +0200

    Floating action menu  button

--- app/src/main/java/com/alpehe/huertovalenciano/MainActivity.java
+++ app/src/main/java/com/alpehe/huertovalenciano/MainActivity.java
@@ -2,10 +2,12 @@ package com.alpehe.huertovalenciano;
 
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.github.clans.fab.FloatingActionButton;
+import com.github.clans.fab.FloatingActionMenu;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.tabs.TabLayout;
 
+import androidx.appcompat.widget.Toolbar;
 import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -26,16 +28,33 @@ public class MainActivity extends AppCompatActivity {
         viewPager.setAdapter(sectionsPagerAdapter);
         TabLayout tabs = findViewById(R.id.tabs);
         tabs.setupWithViewPager(viewPager);
-        FloatingActionButton fab = findViewById(R.id.fab);
-
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        FloatingActionMenu faMenu = findViewById(R.id.faMenu);
+        faMenu.setClosedOnTouchOutside(true);
 
 
-                Snackbar.make(view, "Botón para añadir plantas", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        FloatingActionButton fabAnyadir = findViewById(R.id.fabAnyadir);
+        fabAnyadir.setOnClickListener(view -> Snackbar.make(view, "Botón para añadir plantas", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show());
+
+        FloatingActionButton fabAjustes = findViewById(R.id.fabAjustes);
+        fabAjustes.setOnClickListener(view -> Snackbar.make(view, "Botón para añadir plantas", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show());
     }
+
+    /*@Override public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true; *//** true -> el menú ya está visible *//*
+    }
+
+    @Override public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        if (id == R.id.acercaDe) {
+            *//*lanzarAcercaDe(null);*//*
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }*/
 }
